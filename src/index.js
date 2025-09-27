92bbfaa6380e0c4a2a002c7d4c9b4d489cfdaab2
김성주 <aaa8757k@gmail.com>
2025-09-27 15:13:40 +0900

    refacor: window의 속성으로 SeongJoo추가(불필요한 임포트 제거)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ export const SeongJoo = {
   render,
 };
 
+window.SeongJoo = SeongJoo;
+// import { _SeongJoo } from './index.js';
+// 원래 위와 같이 직접 사용하지 않더라도 바벨이 트랜스파일링 하려면 임포트가 필요(리액트 17버전 이전엔 react를 임포트했던 것처럼)
+// window의 속성으로 추가함으로써 임포트 없이 사용하고자 함
+
+
 function createElement(type, props, ...children) {
   return {
     type,
