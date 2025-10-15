0be0199e9d3214140261c2fbc09e435fd51a7761
626-ju <>
2025-10-15 16:00:51 +0900

    feat:Album페이지 반응형 추가

--- src/pages/Album/Album.js
+++ src/pages/Album/Album.js
@@ -5,23 +5,25 @@ import PolaroidFrame from './PolaroidFrame.js';
 
 const Album = ({ params }) => {
   const { albumId } = params;
-  console.log(albumId)
-  
+  console.log(albumId);
+
   return (
     <div className="bg-background">
       <div
         className="min-h-160 bg-cover bg-center"
         style={{ backgroundImage: `url(${Background})` }}
       >
-        <div className="pt-30 flex gap-10 pb-20 mx-auto w-200 xl:w-280">
+        <div className="pt-10 md:pt-30 flex gap-5 pb-20 mx-auto flex-col md:flex-row min-w-90 md:w-180 xl:w-280">
           {/* 추후 사진 클릭 시 전체 화면으로 볼 수 있게 띄우기 */}
           <PolaroidFrame
             title={''}
-            className={'w-120 h-100 md:w-120 md:h-100 shadow-xl p-5'}
+            className={
+              'w-90 h-75 md:w-120 md:h-100 md:w-120 md:h-100 shadow-xl p-5'
+            }
           />
-          <div>
-            <h2 className="text-2xl mt-5">사진 제목</h2>
-            <p className="mt-10 text-xl">
+          <div className="mx-auto w-90 md:w-120">
+            <h2 className="text-xl md:text-2xl mt-5">사진 제목</h2>
+            <p className="text-md mt-10 md:text-xl">
               사진에 대한 추억 및 설명 사진에 대한 추억 및 설명 사진에 대한 추억
               및 설명 사진에 대한 추억 및 설명
             </p>
--- src/pages/Album/Albums.js
+++ src/pages/Album/Albums.js
@@ -4,7 +4,7 @@ const Albums = () => {
   const res = Array(5).fill(0);
   return (
     <div className="min-w-[375px] pt-40 bg-background">
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-10 mx-auto w-90 md:w-200 xl:w-280">
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-10 mx-auto w-90 md:w-190 xl:w-280">
         {res.map(() => {
           return <PolaroidFrame title={'임시 제목입니다.11111111111111111'} />;
         })}
